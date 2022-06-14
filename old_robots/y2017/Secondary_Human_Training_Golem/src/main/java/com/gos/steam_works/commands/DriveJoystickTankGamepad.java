af5bee4f1cc6171f8e1597bf77ea28bc44683da1
PJ Reiniger <pj.reiniger@gmail.com>
2022-06-14 02:24:11 -0400

    2017 robot

---
+++ old_robots/y2017/Secondary_Human_Training_Golem/src/main/java/com/gos/steam_works/commands/DriveJoystickTankGamepad.java
@@ -0,0 +1,23 @@
+package com.gos.steam_works.commands;
+
+import com.gos.steam_works.subsystems.Chassis;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
+
+/**
+ *
+ */
+public class DriveJoystickTankGamepad extends DriveJoystickBase {
+    private XboxController m_drivingStick;
+
+    public DriveJoystickTankGamepad(XboxController drivingGamepad, Chassis chassis) {
+        super(chassis);
+        m_drivingStick = drivingGamepad;
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    @Override
+    public void execute() {
+        m_chassis.tankDriveTele(-m_drivingStick.getLeftY(), -m_drivingStick.getRightY());
+    }
+}
