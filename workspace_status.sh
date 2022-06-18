4a5bc0be6e0fc8dfd6f176b700c9c55591334fa1
PJ Reiniger <pj.reiniger@gmail.com>
2022-06-18 17:08:48 -0400

    Try again

--- workspace_status.sh
+++ workspace_status.sh
@@ -27,8 +27,3 @@ echo "GIT_BRANCH $git_branch"
 
 git_tree_status=$(git diff-index --quiet HEAD -- && echo 'Clean' || echo 'Modified')
 echo "GIT_TREE_STATUS $git_tree_status"
-
-version=$(cat VERSION)
-echo "VERSION $version"
-
-sleep 10s
