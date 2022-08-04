b3850e85219e717f0593d42c5db8e9d7310b5295
Copybara <copybara@example.com>
2022-08-04 15:17:20 -0400

    Project import generated by Copybara.
    GitOrigin-RevId: 947e5f2c75a25a5b1a129b32749caf3d45dc872c

---
+++ y2022/MEPI/src/main/java/com/gos/mepi/rapid_react/commands/AutoClimb.java
@@ -0,0 +1,18 @@
+package com.gos.mepi.rapid_react.commands;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import com.gos.mepi.rapid_react.subsystems.ClimberSubsystem;
+
+public class AutoClimb extends SequentialCommandGroup{
+
+    public AutoClimb(ClimberSubsystem climber){
+        // new ClimberCommand(climber, -0.5),
+        // new WaitCommand(0.1),
+        // new ClimbPIDComand(climber, 1),
+        // new WaitCommand(0.6),
+        // new ClimbPIDComand(climber, 0),
+        // new 
+    }
+    
+}
